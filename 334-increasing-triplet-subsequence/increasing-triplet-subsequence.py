b9e1f1b9220e2f8fdcb9473beda29936f67556e3
akshar99 <arastogi23@hotmail.com>
2024-09-24 09:35:19 +0530

    Time: 799 ms (42.08%) | Memory: 32.6 MB (14.80%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -2,18 +2,36 @@ class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         if len(nums) < 3:
             return False
-
         first = second = float('inf')
 
-        for n in nums:
+        for num in nums:
+            if num <= first:
+                first = num
 
-            if n <= first:
-                first = n
-
-            elif n <= second:
-                second = n
+            elif num <= second:
+                second = num
 
             else:
                 return True
 
-        return False 
+        return False
+"""
+This is the output
+
+The logic behind is that we should find the third element that is bigger that second
+and second is bigger than first, if it encounters any number that is bigger than second it 
+return TRUE
+
+First Iteration:
+    1
+    first
+    inf
+
+Second Iteration:
+    2
+    second
+    inf
+
+Third Iteration:
+3
+"""
