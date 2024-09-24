1924b7294a0c15aff9beb46357a45a37aa5e7004
akshar99 <arastogi23@hotmail.com>
2024-09-24 09:29:32 +0530

    Time: 792 ms (52.80%) | Memory: 32.7 MB (14.80%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -17,23 +17,3 @@ class Solution:
                 return True
 
         return False 
-"""
-This is the output
-
-The logic behind is that we should find the third element that is bigger that second
-and second is bigger than first, if it encounters any number that is bigger than second it 
-return TRUE
-
-First Iteration:
-    1
-    first
-    inf
-
-Second Iteration:
-    2
-    second
-    inf
-
-Third Iteration:
-3
-"""
