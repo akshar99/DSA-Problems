f934d940484bc18b5a1c6e9b8e2af7ef004625d8
akshar99 <arastogi23@hotmail.com>
2024-09-28 17:39:00 +0530

    Time: 46 ms (37.83%) | Memory: 17 MB (33.42%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target not in nums:
+            return -1
+
+        for index, num in enumerate(nums):
+            if num == target:
+                out = index
+                
+        return out
