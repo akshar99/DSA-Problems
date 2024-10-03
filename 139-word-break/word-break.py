543197beeded282ff10fdddaf362aaa4a656bbe1
akshar99 <arastogi23@hotmail.com>
2024-10-03 10:46:30 +0530

    Time: 35 ms (84.92%) | Memory: 16.7 MB (26.15%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,11 +1,11 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s) + 1)
-        dp[0] = True
+        dp = [False] * (len(s) + 1)#inititate a false array that we can modify later
+        dp[0] = True #first segment will always be true as it is empty
 
-        for i in range(1, len(s)+1):
+        for i in range(1, len(s)+1): #iterate in i this will help us update the elemnts of dp as true or false
             for word in wordDict:
-
+            #we are checking if the substring just before the currrent string can be segmentes
                 if i >= len(word) and s[i - len(word): i] == word:
                     if dp[i - len(word)] == True:
                         dp[i] = True
