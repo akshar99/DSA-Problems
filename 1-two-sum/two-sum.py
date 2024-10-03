f86ff73f85aac29573acd3bd0b21b7696548f5d0
akshar99 <arastogi23@hotmail.com>
2024-10-03 11:05:17 +0530

    Time: 55 ms (80.37%) | Memory: 17.7 MB (34.81%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,22 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_map = {}
+        #x + y = target 
+        #target  - x = y
+        #create a hash map here 
+        seen = {}
 
+        for i ,num in enumerate(nums):
+            diff = target - num
 
-        for i, ele in enumerate(nums):
-            complement  = target - ele
-            if complement in hash_map:
-                return [hash_map[complement], i]
-            hash_map[ele] = i
+            if diff in seen:
+                return(seen[diff], i)
 
-
-"""
-BRUTE FORCE Failed approacj
-        for ele in nums:
-            hash_map(ele) = target - ele
-
-        for key, val in enumerate(hash_map):
-            if val in nums:
-                return [key, val]
-
-"""
+            seen[num] = i
