390f651f830a82733f4d05843a9f127a0eae1edb
akshar99 <arastogi23@hotmail.com>
2024-10-04 11:41:59 +0530

    Time: 58 ms (98.46%) | Memory: 16.7 MB (13.18%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,34 +1,33 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        result = []
-        nums.sort()
 
+        result  = []
+        nums.sort()
         n = len(nums)
 
-        for i in range(n - 3):
+        for i in range(n-3):
+            if nums[i] + nums[i+1] + nums[i + 2] + nums[i + 3] > target: 
+                break #is sum is > target
 
-            if nums[i] + nums[i+1] + nums[i + 2] + nums[i + 3] > target:
-                break
-
-            if nums[i] + nums[n - 1] + nums[n - 2] + nums[n- 3] < target:
+            if nums[i] + nums[n- 1] + nums[n - 2] + nums[n - 3] < target:
                 continue
 
-            if i > 0 and nums[i] == nums[i -1]:
+            if i > 0 and nums[i] == nums[i -1]: #skip if duplicates
                 continue
 
             for j in range(i+1, n-2):
 
-                if j > i + 1 and nums[j] == nums[j-1]:
-                    continue
-
                 if nums[i] + nums[j] + nums[j+1] + nums[j+2] > target:
-                    break
+                    break # break if sum > target
 
-                if nums[i] + nums[j] + nums[n-1] + nums[n -2] < target:
+                #check for duplicates
+                if j > i +1 and nums[j] == nums[j- 1]:
                     continue
 
+                if nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target:
+                    continue
 
-                left, right = j + 1, n - 1
+                left, right = j + 1, n-1
 
                 while left < right:
 
@@ -42,8 +41,8 @@ class Solution:
                         while left < right and nums[left] == nums[left -1]:
                             left += 1
 
-                        while left < right and nums[right] == nums[right+ 1]:
-                            right -= 1
+                        while left < right and nums[right] == nums[right +1]:
+                            right -=1
 
                     elif total < target:
                         left += 1
@@ -52,3 +51,6 @@ class Solution:
                         right -= 1
 
         return result
+
+
+                    
