202a73186a622a0e537083823582b8ac2ef7735b
akshar99 <arastogi23@hotmail.com>
2024-10-14 11:11:21 +0530

    Time: 25 ms (98.34%) | Memory: 16.6 MB (31.39%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -3,6 +3,15 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+"""
+Initiate a dummy node and slow fast to it 
+increment the fast pointer by n steps
+now start incrementing slow and fast together 
+
+when loops end slow will be just one point before desored point ot be reomved 
+
+inititate the next to next pointer for slow and isolate the point to bne removed
+"""
 
 
 class Solution:
@@ -11,7 +20,7 @@ class Solution:
         dummy = ListNode(0, head)
 
         slow=fast= dummy
-
+        #increment fast already n steps ahead
         for _ in range(n):
             fast = fast.next
 
