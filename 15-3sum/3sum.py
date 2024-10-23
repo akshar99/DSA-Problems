b3ac7dfa3b54f92a52da7342536d07dbee53e2c7
akshar99 <arastogi23@hotmail.com>
2024-10-23 12:47:27 +0530

    Time: 603 ms (80.65%) | Memory: 19.8 MB (97.12%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,33 +1,40 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        #need to calculate sum of three int , where i!=j, j!=k, k!=i
+        """upon transversing the array we also need to skip duplicates
+        first thing to do is to sort the array and start the loop with i in range(len(nums))
+        left is +1 i and right is last 
+        """ 
+
         nums.sort()
         out = []
-        
+
         for i in range(len(nums)):
 
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0  and nums[i] == nums[i - 1]:
                 continue
 
-            left , right = i + 1, len(nums) - 1
+            l = i +1
+            r = len(nums) - 1
 
-            while left < right:
-                sums = nums[i] + nums[left] + nums[right]
+            while l < r:
+                sums = nums[i] + nums[l] + nums[r]
+
                 if sums == 0:
-                    out.append([nums[i], nums[left], nums[right]])
-                    left += 1
+                    out.append([nums[i], nums[l], nums[r]])
+                    l += 1
 
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    r -= 1
 
-                    right -= 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
 
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
                 elif sums < 0:
-                    left += 1
+                    l += 1
 
                 else:
-                    right -= 1
-        return out
-                
-                
+                    r -= 1
+
+        return out 
