c44393e20f067240bff2130ce11ace88b3dd1d3e
akshar99 <arastogi23@hotmail.com>
2024-10-24 16:07:54 +0530

    Time: 4 ms (94.80%) | Memory: 18.5 MB (5.38%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -4,13 +4,16 @@ class Solution:
         chars = ['+', '-', '*', '/']
         
         for t in tokens:
+            #check if not arithmetic operators append the numbers
             if t not in chars:
                 stack.append(t)
 
             else:
+                #pop 2 values for operations
                 b = int(stack.pop())
                 a = int(stack.pop())
 
+                #perform operations as per operations
                 if t == '+':
                     stack.append(a + b)
 
