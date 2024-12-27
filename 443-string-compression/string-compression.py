077284a81c00cca39145df13a9a3987c5ce33207
akshar99 <arastogi23@hotmail.com>
2024-12-27 08:50:40 +0530

    Time: 4 ms (14.24%) | Memory: 17.8 MB (7.57%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,23 +1,24 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
         #initialize two pointer write and i
-        write = 0 #write for writing
-        i = 0 #i for iterating
+        write = 0
+        i = 0
 
         while i < len(chars):
-            char = chars[i] #
+            char = chars[i]
             count = 0
 
             while i < len(chars) and chars[i] == char:
-                count += 1 # if the count is found increase the counter
-                i += 1 # increase the iterator 
+                count += 1
+                i += 1
 
-            chars[write] = char #write the character in the list
-            write += 1 #increase the write counter
+            chars[write] = char
+            write += 1
 
-            if  count >1: #only greater than one needs to be written to chars
-                for c in str(count): #taking count as a str to write it into chars
+            if count > 1:
+                for c in str(count):
                     chars[write] = c
                     write += 1
 
         return write
+
